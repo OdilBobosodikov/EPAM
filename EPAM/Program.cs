fae5db5506f14ac03930b9cad9318a7fd2e96b33
OdilBobosodikov <odilbobosodikov2002.com>
2023-04-16 15:56:23 +0500

    Initial Commit

--- EPAM/Program.cs
+++ EPAM/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Automated Testing Foundations with .NET");
         }
     }
 }
